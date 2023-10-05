fb6c5da13909544117104612cc31c3f5eb780ac7
Ziyu Wang <wang0935@algonquinlive.com>
2023-10-05 17:16:14 -0400

    Finished Phone Version of Lab 3

--- app/src/main/java/algonquin/cst2335/wang0935/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/wang0935/MainActivity.java
@@ -3,6 +3,12 @@ package algonquin.cst2335.wang0935;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Switch;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        View imgView = findViewById(R.id.imageView);
+        Switch sw = findViewById(R.id.spin_switch);
+
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
+            }
+            else {
+                imgView.clearAnimation();
+            }
+        });
     }
-}
+    }
+
