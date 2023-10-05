59876e5bbc7531ce5384934730c6d432876a1cfd
Ziyu Wang <wang0935@algonquinlive.com>
2023-10-05 17:58:56 -0400

    Finished TV app

--- app/src/main/java/algonquin/cst2335/wang0935/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/wang0935/MainActivity.java
@@ -19,23 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         View imgView = findViewById(R.id.imageView);
-        Switch sw = findViewById(R.id.spin_switch);
 
-        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
-
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
-
-                imgView.startAnimation(rotate);
-            }
-            else {
-                imgView.clearAnimation();
-            }
-        });
+        };
     }
-    }
+
 
